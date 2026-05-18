72b93b23a4609b5841f33bcef187e395346cf8bb
Anatolii Bazko <abazko@redhat.com>
2026-05-18 11:29:38 +0200

    fixup
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/handlers/ok_pr_review_handler.go
+++ pkg/handlers/ok_pr_review_handler.go
@@ -13,14 +13,9 @@ package handlers
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"strings"
 
 	"github.com/tolusha/che-doc-generator/pkg/github"
 )
-
-const outputDelimiter = "===OK-PR-REVIEW-OUTPUT==="
 
 type OkPRReviewHandler struct{}
 
@@ -34,54 +29,4 @@ func (g *OkPRReviewHandler) OnSuccess(
 	trigger *github.Trigger,
 	gitHubClient *github.Client,
 ) {
-	parts := strings.Split(result, outputDelimiter)
-
-	var reviews []string
-	for _, part := range parts {
-		trimmed := strings.TrimSpace(part)
-		if trimmed != "" {
-			reviews = append(reviews, trimmed)
-		}
-	}
-
-	if len(reviews) < 4 {
-		body := fmt.Sprintf("%s\n\nPullRequest review not found.", trigger.CommentBody)
-
-		if err := gitHubClient.UpdatePullRequestComment(
-			ctx,
-			trigger.Owner,
-			trigger.Repo,
-			trigger.CommentID,
-			body,
-		); err != nil {
-			log.Printf(
-				"[ERROR] Failed to post on %s/%s#%d: %v",
-				trigger.Owner,
-				trigger.Repo,
-				trigger.PRNumber,
-				err,
-			)
-		}
-
-		return
-	}
-
-	for _, output := range reviews {
-		err := gitHubClient.PostPullRequestComment(
-			ctx,
-			trigger.Owner,
-			trigger.Repo,
-			trigger.PRNumber,
-			output,
-		)
-		if err != nil {
-			log.Printf(
-				"[ERROR] Failed to post on %s/%s#%d: %v",
-				trigger.Owner,
-				trigger.Repo,
-				trigger.PRNumber,
-				err,
-			)
-		}
-	}
 }
